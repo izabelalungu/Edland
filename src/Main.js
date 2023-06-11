f8189187cbcbb3b29fac18f8edcf9d61d173cbe8
Izabela <izabelalungu9@gmail.com>
2023-06-12 00:45:01 +0300

    v.0.5 added sectios elements

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import './Main.css';
 import { useState, useEffect } from 'react';
+// import Gallery from './components/Gallery';
+import Carousel from 'react-bootstrap/Carousel';
+
 import slide1 from "./images/5.jpg";
 import slide2 from "./images/8.jpg";
 import slide3 from "./images/23.jpg";
 import designfull from "./images/design_site_full.png";
-// import circleimg from "./images/poza_cerc_full.png"
-import circleimg from "./images/poza_cerc_fullb.png"
+import circleimg from "./images/poza_cerc_fullb.png";
 
+import image1 from './images/Galerie/CABIN_S02/CABIN S02/1.jpg';
+import image2 from './images/Galerie/CABIN_S02/CABIN S02/2.jpg';
+import image3 from './images/Galerie/CABIN_A01/CABIN A01/1.png';
+import image4 from './images/Galerie/CABIN_A01/CABIN A01/2.png';
+import image5 from './images/Galerie/CABIN_A02/CABIN A02/1.jpg';
+import image6 from './images/Galerie/CABIN_A02/CABIN A02/2.jpg';
+
+const Gallery = () => {
+  const projects = [
+    {
+      id: 1,
+      title: 'Project 1',
+      images: [image3,image4],
+      description: 'This is the description for Project 1.'
+    },
+    {
+      id: 2,
+      title: 'Project 2',
+      images: [image5,image6],
+      description: 'This is the description for Project 2.'
+    },
+    {
+      id: 3,
+      title: 'Project 3',
+      images: [image1, image2],
+      description: 'This is the description for Project 3.'
+    }
+  ];
+
+  return (
+    <div className="container">
+      <div className="row">
+        {projects.map((project) => (
+          <div key={project.id} className="col-md-4">
+            <h3>{project.title}</h3>
+            <Carousel>
+              {project.images.map((image, index) => (
+                <Carousel.Item key={index}>
+                  <img
+                    className="d-block w-100"
+                    src={image}
+                    alt={`Slide ${index + 1}`}
+                  />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const SlideshowBackground = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -79,23 +134,47 @@ const SlideshowBackground = () => {
   );
 };
 
+const AboutUs = () => {
+  return (
+    <div className="container-panels">
+      <div className="justify-content-evenly align-items-center display-md-devices-aboutus">
+        <div className="container-box">First</div>
+        <div className="container-box-middle">Middle</div>
+        <div className="container-box">Third</div>
+      </div>
+    </div>
+  );
+};
+
 function Main() {
   return (
     <main className="main">
       <SlideshowBackground />
+      <AboutUs/>
+
       <div className="imagine_mare_container">
         <img src={designfull} alt="About us" className="imagine_mare_style" />
       </div>
       <div className="text_under_bigimg_container">
         <p>aici vine un text despre servicii si despre about us</p>
       </div>
+
       <div className="prices_panel_container">
-        <p>aici vine un panou super cute cu reclama</p>
-      </div>
-      <div className="circle_img_container">
-        <img src={circleimg} alt="Descopera acum lumea caselor modulare" className="imagine_mare_style" />
+        <div className="row justify-content-center align-items-center rectangle">
+          <div className="col-1 dotted-line"></div>
+        </div>
       </div>
 
+      <div class="bbl_img_container">
+        <div class="imageContainer">
+          <img src={circleimg} alt=""/>
+        </div> 
+        <div class="text_leftbbl">
+          <p></p>
+        </div>
+      </div>
+
+      <Gallery/>
     </main>
   );
 }
